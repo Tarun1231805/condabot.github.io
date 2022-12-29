bd39662a572dcba9c85bceb2eade77e8cb27f215
Tarun1231805 <65174896+Tarun1231805@users.noreply.github.com>
2022-12-29 19:32:53 +0530

    Add files via upload

---
+++ scripts/anaconda-navigator-app.bat
@@ -0,0 +1,3 @@
+chcp 1252
+call C:\Users\Tarun\anaconda3\Scripts\activate C:\Users\Tarun\anaconda3
+anaconda-navigator >C:\Users\Tarun\.anaconda\navigator\scripts\anaconda-navigator-app-out-1.txt 2>C:\Users\Tarun\.anaconda\navigator\scripts\anaconda-navigator-app-err-1.txt
